2869559d48e4bb360bf9f1fae02f120f735eb06d
Craig Gant <craigkgant@gmail.com>
2019-11-30 14:19:12 -0500

    added loop to ensure vaild password length

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,57 @@
 
 //ARRAYS needed for the Random Password Generator
 
-//All lowercase letters
-var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-//All capital letters
-var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-//All numeric characters
-var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-//All special characters
-var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "`", "{", "|", "}", "~", "_", "[", "]", "^"];
+  //All lowercase letters
+  var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  //All capital letters
+  var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  //All numeric characters
+  var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  //All special characters
+  var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "`", "{", "|", "}", "~", "_", "[", "]", "^"];
 
 
 // STAGE 1—— Initial prompts
 
-// This DO loop ensures that at least one type of character is chosen.
-do {
-  // Lowercase letter confirm
-  var lowercaseConfirm = confirm("Would you like your password to contain lowercase letters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
-    console.log(lowercaseConfirm);
-  }
-  // Capital letter confirm
-  var capitalConfirm = confirm("Would you like your password to contain capital letters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
-    console.log(capitalConfirm);
-  }
-  // Numeric character confirm
-  var numeralConfirm = confirm("Would you like your password to contain numeric characters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
-    console.log(numeralConfirm);
-  }
-  // Special character confirm
-  var specialConfirm = confirm("Would you like your password to contain special characters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
-    console.log(specialConfirm);
-  }
-    if ((lowercaseConfirm === false) && (capitalConfirm === false) && (numeralConfirm === false) && (specialConfirm === false)){
-      alert("Passwords must include at least one type of character. \nPlease review the options again, and select the types of characters you would like to include.");
+  // This do loop ensures that at least one type of character is chosen.
+  do {
+    // Lowercase letter confirm
+    var lowercaseConfirm = confirm("Would you like your password to contain lowercase letters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
+      console.log(lowercaseConfirm);
     }
-} while ((lowercaseConfirm === false) && (capitalConfirm === false) && (numeralConfirm === false) && (specialConfirm === false))
+    // Capital letter confirm
+    var capitalConfirm = confirm("Would you like your password to contain capital letters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
+      console.log(capitalConfirm);
+    }
+    // Numeric character confirm
+    var numeralConfirm = confirm("Would you like your password to contain numeric characters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
+      console.log(numeralConfirm);
+    }
+    // Special character confirm
+    var specialConfirm = confirm("Would you like your password to contain special characters? \nPlease select 'ok' for yes, and 'cancel' for no."); {
+      console.log(specialConfirm);
+    }
+      if ((lowercaseConfirm === false) && (capitalConfirm === false) && (numeralConfirm === false) && (specialConfirm === false)) {
+        alert("Passwords must include at least one type of character. \nPlease review the options again, and select the types of characters you would like to include.");
+      }
+  } while ((lowercaseConfirm === false) && (capitalConfirm === false) && (numeralConfirm === false) && (specialConfirm === false));
 
+  // This do loop ensures that a valid password length is chosen.
+  do {
+    // Ask the user how many characters they would like in the password.
+    var userPasswordLength = prompt ("How long would you like your password to be? \nPlease select a number between 8 and 128");
+    // Makes sure that user input is interpreted as a number.
+    var passwordLength = parseInt(userPasswordLength); {
+      console.log(passwordLength);
+    }
+    //Correct if desired password length is invalid.
+    if ((passwordLength > 128) || (passwordLength < 8)) {
+      alert("Passwords must be between 8 and 128 characters in length. \nWhen prompted, please enter a valid number.");
+    }   
+  } while ((passwordLength > 128) || (passwordLength < 8));
 
+  
+// STAGE 2—— Randomly selecting the password components
 
 
 
