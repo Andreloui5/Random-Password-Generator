e91348f49cb7f627f94acaac402aa92e5bf45a83
Craig Gant <craigkgant@gmail.com>
2019-11-30 11:25:56 -0500

    Added arrays: lowercase letter, capital letter, numerals, and special characters

--- Develop/script.js
+++ Develop/script.js
@@ -1,11 +1,17 @@
 
-//arrays
-var lowercaseLetters ["a","b", etc.]
+//ARRAYS needed for the Random Password Generator
 
+//All lowercase letters
+var lowercaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+//All capital letters
+var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+//All numeric characters
+var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+//All special characters
+var specialCharacters = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "`", "{", "|", "}", "~", "_", "[", "]", "^"];
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 
 // Write password to the #password input
 function writePassword() {
